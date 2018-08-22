9916c8491f6ada7cb80b436555f7a1e7c8ff8299
VolodymyrRomanytsia <romanyca@gmail.com>
2018-08-22 23:46:28 +0300

    Fixed JS

--- js/script.js
+++ js/script.js
@@ -1,21 +1,25 @@
-const button = document.querySelector('.button');
-button.onclick = () => {
-	let recursion = (number) => {
-    	if (number > 0) {
-    	const container = document.body.children[0];	
+const start = () => { 
+	const element = number => {
+		const container = document.body.children[0];	
 		const block = container.children[1];
 		const ul = block.children[0];
-		let li = document.createElement('li');
-		let size = number * 10 +'px';
+		const li = document.createElement('li');
+		const size = number * 10 +'px';
 		li.style.fontSize = size;
 		ul.appendChild(li);
 		li.insertAdjacentHTML("afterBegin", number);
-        console.log(number);
+	}
+
+	const recursion = number => {
+    	if (number > 0) {
+		element(number);
         return recursion(number - 1);
     	} else {
         return number;
 	    }
 	}
 	recursion(10);
-
 }
+
+const button = document.querySelector('.button');
+button.addEventListener('click', start); 
